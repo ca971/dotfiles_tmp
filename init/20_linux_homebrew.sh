c8e5b48b14a891b776c6b601cb321b469809d6b7
ca971 <discretgwad@gmail.com>
2021-09-10 23:05:13 +0200

    Removed jakewemaere geo tap

--- init/20_linux_homebrew.sh
+++ init/20_linux_homebrew.sh
@@ -1,15 +1,12 @@
 # Source Homebrew
 source $DOTFILES/source/50_linux_homebrew.sh
 
-#eval $(~/.homebrew/bin/brew shellenv)
-
 # Install Homebrew
 if [[ ! -d "~/.homebrew" ]]; then
   git clone https://github.com/Homebrew/brew.git ~/.homebrew 
   grep --silent "~/.homebrew/bin" <<< $PATH || export PATH="~/.homebrew/bin:$PATH"
 
   brew tap buo/cask-upgrade
-  brew tap jakewmeyer/geo
   brew tap neovim/neovim
   brew tap universal-ctags/universal-ctags
   brew tap homebrew/aliases
