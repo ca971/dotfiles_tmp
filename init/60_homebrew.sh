a73cadd381aa34986d345c7c3852271b71aae408
ca971 <discretgwad@gmail.com>
2021-09-10 03:09:42 +0200

    remove hombrew-core tap

--- init/60_homebrew.sh
+++ init/60_homebrew.sh
@@ -1,13 +1,13 @@
 # Source Homebrew
 source $DOTFILES/source/60_homebrew.sh
 
+#eval $(~/.homebrew/bin/brew shellenv)
+
 # Install Homebrew
 if [[ ! -d "~/.homebrew" ]]; then
   git clone https://github.com/Homebrew/brew.git ~/.homebrew 
   grep --silent "~/.homebrew/bin" <<< $PATH || export PATH="~/.homebrew/bin:$PATH"
-  eval $(~/.homebrew/bin/brew shellenv)
 
-  brew tap homebrew/core
   brew tap buo/cask-upgrade
   brew tap jakewmeyer/geo
   brew tap neovim/neovim
