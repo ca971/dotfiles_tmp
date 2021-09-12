f3ddc7a67e02d0ebd15b3908c5c3c877afaa8c7d
ca971 <discretgwad@gmail.com>
2021-09-12 14:34:18 +0200

    Modified init/90_linux_homebrew.sh

--- init/90_linux_homebrew.sh
+++ init/90_linux_homebrew.sh
@@ -6,3 +6,6 @@ test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >> ~/.bash_profile
 echo "eval \$($(brew --prefix)/bin/brew shellenv)" >> ~/.profile
+
+brew doctor
+brew install hello
