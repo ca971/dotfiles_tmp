2a16370d707596c411ffff1aa87d60f670b32620
ca971 <discretgwad@gmail.com>
2021-09-12 23:08:58 +0200

    Modified init/50_docker.sh

--- init/50_docker.sh
+++ init/50_docker.sh
@@ -1,22 +1,14 @@
 # sudo caches the password for a few minutes
 sudo true
 
-# Install Docker
-sudo apt-get update -y
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+# Install Docker from the official docker install script
+wget -qO- https://get.docker.com/ | sh
+
+# Install Docker Compose
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
-
-
-# Install Docker Compose
-compose_release() {
-  curl --silent "https://api.github.com/repos/docker/compose/releases/latest" |
-  grep -Po '"tag_name": "\K.*?(?=")'
-}
-
-if ! [ -x "$(command -v docker-compose)" ]; then
-  curl -L https://github.com/docker/compose/releases/download/$(compose_release)/docker-compose-$(uname -s)-$(uname -m) \
-  -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
-fi
