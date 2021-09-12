6ed40e4036fe438bed869294424b6536482722fc
ca971 <discretgwad@gmail.com>
2021-09-12 14:10:23 +0200

    Modified init/90_linux_homebrew.sh

--- init/90_linux_homebrew.sh
+++ init/90_linux_homebrew.sh
@@ -1,5 +1,5 @@
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Add Homebrew into PATH
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
