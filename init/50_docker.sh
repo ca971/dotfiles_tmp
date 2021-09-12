c8c76815db0a1d27271e2f4e7a9e5c8a8cace74e
ca971 <discretgwad@gmail.com>
2021-09-12 15:16:20 +0200

    Modified init/50_docker.sh and add docker-compose

--- init/50_docker.sh
+++ init/50_docker.sh
@@ -1,3 +1,6 @@
+# sudo caches the password for a few minutes
+sudo true
+
 # Install Docker
 sudo apt-get update -y
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -5,3 +8,15 @@ sudo sh get-docker.sh
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+
+# Install Docker Compose
+compose_release() {
+  curl --silent "https://api.github.com/repos/docker/compose/releases/latest" |
+  grep -Po '"tag_name": "\K.*?(?=")'
+}
+
+if ! [ -x "$(command -v docker-compose)" ]; then
+  curl -L https://github.com/docker/compose/releases/download/$(compose_release)/docker-compose-$(uname -s)-$(uname -m) \
+  -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
+fi
